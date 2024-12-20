1e04e60875f227e8db09ab91ad49fef0009887fa
Raihan <mraihanm99@gmail.com>
2024-12-20 10:03:43 +0800

    feat(models): add models and migrations
    - Set up Eloquent models and migrations for:
      -> BoardingHouse
      -> Bonus
      -> Category
      -> City
      -> Room
      -> RoomImage
      -> Testimonial
      -> Transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function boardingHouse()
+    {
+        return $this->belongsTo(BoardingHouse::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+}
