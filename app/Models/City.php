fea724be357c3d6461c673d308cdc654a6d280d3
Raihan <mraihanm99@gmail.com>
2024-12-21 15:03:51 +0800

    fix: delete associated images when a City is deleted

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,18 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class City extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'image',
-        'name',
-        'slug'
-    ];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    protected static function booted()
+    {
+        static::deleting(function ($city) {
+            $image = $city->image;
+            $publicStorage = Storage::disk('public');
+
+            if ($image && $publicStorage->exists($image)) {
+                $publicStorage->delete($image);
+            }
+        });
+    }
 
     public function BoardingHouses()
     {
