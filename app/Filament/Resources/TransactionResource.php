6b0a5ed8b0c295e3f7c0f07b2de7cd9589dad153
Raihan <mraihanm99@gmail.com>
2025-01-01 02:17:18 +0800

    refactor: change icon and organize navigations

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -25,7 +25,9 @@ class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-credit-card';
+
+    protected static ?string $navigationGroup = 'Boarding House Management';
 
     public static function form(Form $form): Form
     {
